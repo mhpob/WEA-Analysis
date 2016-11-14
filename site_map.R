903d0879f7775b6be4fa5f44b172429bede911db
mhpob <mikeob9@gmail.com>
2016-11-14 14:00:40 -0500

    Create site_map.R

---
+++ site_map.R
@@ -0,0 +1,22 @@
+sites <- read.csv('sites.csv')
+
+library(raster)
+midstates <- shapefile('p:/obrien/midatlantic/matl_states_land.shp')
+wea <- shapefile('c:/users/secor/desktop/gis products/md mammals/wind_planning_areas/Wind_Planning_Areas_06_20_2014.shp')
+
+library(ggplot2)
+midstates <- fortify(midstates)
+wea <- fortify(wea)
+
+ggplot() +
+  geom_point(data = sites[sites$Type == 'Actual',], aes(x = long, y = lat)) +
+  geom_text(data = sites[sites$Type == 'Actual',], aes(x = long, y = lat,
+                                  label = ID, vjust = 1.2))+
+  geom_polygon(data = midstates, aes(x = long, y = lat, group = group),
+                        fill  = 'lightgrey', color = 'black') +
+  geom_polygon(data = wea, aes(x = long, y = lat, group = group),
+               fill = NA, color = 'black') +
+  coord_map(xlim = c(-75.2, -74.45), ylim = c(38.2, 38.5)) +
+  labs(x = 'Longitude', y = 'Latitude') +
+  theme_bw()
+
