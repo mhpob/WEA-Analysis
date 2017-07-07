0a0e0ec39a05bb5a6e546519fb8347eb919cd916
mhpob <mikeob9@gmail.com>
2017-07-07 11:39:48 -0400

    Create a plot aggregated across months.

--- temp v ecdf.R
+++ temp v ecdf.R
@@ -9,39 +9,38 @@ load('p:/obrien/randomr/ACTactive.rda')
 species <- left_join(data.frame(dets), ACTactive,
                      by = c('transmitter' = 'Tag.ID.Code.Standard')) %>%
   mutate(Common.Name = ifelse(grepl('striped', Common.Name, ignore.case = T),
-                              'Striped bass', Common.Name),
-         month = lubridate::month(date.utc, label = T, abbr = T),
+                              'Striped bass', Common.Name)) %>%
+  group_by(array, Common.Name, transmitter) %>%
+  distinct(array, transmitter, .keep_all = T) %>%
+  summarize(min = min(date.utc))
+
+AS <- filter(species, grepl('stur', Common.Name, ignore.case = T))
+
+
+## Same thing, but for month/array combinations ----
+species_mo <- species %>%
+  mutate(month = lubridate::month(date.utc, label = T, abbr = T),
          month = factor(month,
                         levels = c('Nov', 'Dec', 'Jan', 'Feb', 'Mar'))) %>%
   group_by(array, Common.Name, transmitter, month) %>%
   distinct(array, transmitter, .keep_all = T) %>%
   summarize(min = min(date.utc))
 
-AS <- filter(species, grepl('stur', Common.Name, ignore.case = T))
-SB <- filter(species, grepl('striped', Common.Name, ignore.case = T))
+AS_mo <- filter(species_mo, grepl('stur', Common.Name, ignore.case = T))
 
-AS <- split(AS, list(AS$array, AS$month))
-AS <- AS[lapply(AS, function(x){dim(x)[1]}) > 0]
-AS <- lapply(AS, function(x){ecdf(x$min)})
-CF_data$date.local <- as_datetime(CF_data$x, tz = 'America/New_York')
+# Split according to array/month combos
+AS_mo <- split(AS_mo, list(AS_mo$array, AS_mo$month))
+
+# Select non-empty parts of list
+AS_mo <- AS_mo[lapply(AS_mo, function(x){dim(x)[1]}) > 0]
+
+# Apply ecdf to list
+AS_mo <- lapply(AS_mo, function(x){ecdf(x$min)})
+# plot(AS_mo[[2]], verticals = T)
+
+SB <- filter(species_mo, grepl('striped', Common.Name, ignore.case = T))
 
 
-final_detect_cf <- function(data, pct.stop){
-  new.data <- hi_lo(data)
-
-  #   Note: no built-in function, so I'm going to cheat and use a built-in
-  #   function from the ggplot2 package
-  CF_plot <- ggplot() + stat_ecdf(data = new.data, aes(x = high)) +
-    scale_x_datetime(date_breaks = '3 months')
-  print(CF_plot)
-
-  #   Pull data from the plot. Dates are returned in Unix (seconds since
-  #   Jan 1, 1970), so we need to convert back to a usable time.
-  CF_data <- data.frame(ggplot_build(CF_plot)$data)
-  CF_data$date.local <- as_datetime(CF_data$x, tz = 'America/New_York')
-
-  #   Find date where X% of tags stopped transmitting
-  stopped <- min(CF_data[CF_data$date.local >= pct.stop, 'date.local'])
-  cat((as.numeric(pct.stop) * 100), '% of last detections occurred before',
-      as.character(stopped))
-}
+# Dates are returned in Unix (seconds since Jan 1, 1970), so we need to convert
+# back to a usable time. Will be something like:
+# data$date <- as_datetime(data$date, tz = 'America/New_York')
