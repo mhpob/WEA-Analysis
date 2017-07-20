51e644f3b6418a7e5ba0e7652ac08df8380f3f4a
mhpob <mikeob9@gmail.com>
2017-07-20 14:41:05 -0400

    Misc. detection data summary code.

---
+++ misc report summary.R
@@ -0,0 +1,29 @@
+library(lubridate); library(dplyr)
+
+rec.data <- readRDS("rec_events.rds")
+rec.data <- rec.data %>%
+  filter(Description == 'Average noise',
+         Date.Time > ymd('20161113')) %>%
+  mutate(array = ifelse(grepl('I', Site), 'Inner',
+                        ifelse(grepl('O', Site), 'Outer',
+                               'MD WEA')),
+         Data = as.numeric(Data),
+         day = lubridate::floor_date(Date.Time, 'day')) %>%
+  group_by(day, Site, array) %>%
+  summarize(max = max(Data)) %>%
+  group_by(day, array) %>%
+  summarize(avg = mean(max)) %>%
+  group_by(array) %>%
+  summarize(t = sum(avg > 300))
+
+rec.data <- readRDS("rec_events.rds")
+rec.data <- rec.data %>%
+  filter(Description == 'Tilt angle',
+         Date.Time > ymd('20161113')) %>%
+  mutate(array = ifelse(grepl('I', Site), 'Inner',
+                        ifelse(grepl('O', Site), 'Outer',
+                               'MD WEA')),
+         Data = as.numeric(Data),
+         day = lubridate::floor_date(Date.Time, 'day')) %>%
+  group_by(day, array) %>%
+  summarize(avg = mean(Data))
