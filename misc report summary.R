d10c44e8759a754298c7e992a9505feab1f30538
mhpob <mikeob9@gmail.com>
2017-09-01 16:28:49 -0400

    Noise summary.

--- misc report summary.R
+++ misc report summary.R
@@ -1,20 +1,33 @@
 library(lubridate); library(dplyr)
 
 rec.data <- readRDS("rec_events.rds")
-rec.data <- rec.data %>%
+
+# Noise Summary
+noise.data <- rec.data %>%
   filter(Description == 'Average noise',
          Date.Time > ymd('20161113')) %>%
   mutate(array = ifelse(grepl('I', Site), 'Inner',
                         ifelse(grepl('O', Site), 'Outer',
                                'MD WEA')),
          Data = as.numeric(Data),
-         day = lubridate::floor_date(Date.Time, 'day')) %>%
+         day = lubridate::floor_date(Date.Time, 'day'))
+
+noise.data %>%
   group_by(day, Site, array) %>%
   summarize(max = max(Data)) %>%
   group_by(day, array) %>%
   summarize(avg = mean(max)) %>%
   group_by(array) %>%
   summarize(t = sum(avg > 300))
+
+
+noise.data %>%
+  group_by(day, array) %>%
+  summarize(avg = mean(Data)) %>%
+  group_by(array) %>%
+  summarize(sum(avg > 300))
+
+
 
 rec.data <- readRDS("rec_events.rds")
 rec.data <- rec.data %>%
