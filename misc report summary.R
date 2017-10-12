ac9e3df5d64517849de7032ba80a088a4f52f135
mhpob <mikeob9@gmail.com>
2017-10-12 15:10:12 -0400

    Add temperature summary.

--- misc report summary.R
+++ misc report summary.R
@@ -2,6 +2,30 @@ library(lubridate); library(dplyr)
 
 rec.data <- readRDS("rec_events.rds")
 
+# Temperature summary
+wt.data <- rec.data %>%
+  filter(Description == 'Average temperature',
+         Date.Time > ymd('20161113')) %>%
+  mutate(array = ifelse(grepl('I', Site), 'Inner',
+                        ifelse(grepl('O', Site), 'Outer',
+                               'MD WEA')),
+         Data = as.numeric(Data),
+         day = lubridate::floor_date(Date.Time, 'day'))
+
+wt.data %>%
+  filter(Date.Time > ymd('20170330')) %>%
+  group_by(day, array) %>%
+  summarize(daily = mean(Data))%>%
+  ungroup() %>%
+
+  # group_by(array) %>%
+  summarize(min = min(daily),
+            avg = mean(daily),
+            max = max(daily))
+
+
+
+
 # Noise Summary
 noise.data <- rec.data %>%
   filter(Description == 'Average noise',
@@ -12,15 +36,16 @@ noise.data <- rec.data %>%
          Data = as.numeric(Data),
          day = lubridate::floor_date(Date.Time, 'day'))
 
+# Number of days with loud noise pulses
 noise.data %>%
   group_by(day, Site, array) %>%
   summarize(max = max(Data)) %>%
   group_by(day, array) %>%
   summarize(avg = mean(max)) %>%
   group_by(array) %>%
-  summarize(t = sum(avg > 300))
+  summarize(sum(avg > 300))
 
-
+# Number of days with loud average noise levels
 noise.data %>%
   group_by(day, array) %>%
   summarize(avg = mean(Data)) %>%
@@ -28,15 +53,30 @@ noise.data %>%
   summarize(sum(avg > 300))
 
 
-
-rec.data <- readRDS("rec_events.rds")
-rec.data <- rec.data %>%
+# Tilt summary
+tilt.data <- rec.data %>%
   filter(Description == 'Tilt angle',
          Date.Time > ymd('20161113')) %>%
   mutate(array = ifelse(grepl('I', Site), 'Inner',
                         ifelse(grepl('O', Site), 'Outer',
                                'MD WEA')),
          Data = as.numeric(Data),
-         day = lubridate::floor_date(Date.Time, 'day')) %>%
+         day = lubridate::floor_date(Date.Time, 'day'))
+
+# Mean daily tilt angle
+tilt.data %>%
   group_by(day, array) %>%
-  summarize(avg = mean(Data))
+  summarize(daily = mean(Data))%>%
+  ungroup() %>%
+  summarize(min = min(daily),
+            avg = mean(daily),
+            max = max(daily))
+
+tilt.data %>%
+  group_by(day, array) %>%
+  summarize(daily = mean(Data))%>%
+  group_by(array) %>%
+  summarize(min = min(daily),
+            avg = mean(daily),
+            var = var(daily),
+            max = max(daily))
