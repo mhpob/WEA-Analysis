0cd52bb1574fbdb91e1a0c74b9d2310fff07d79d
mhpob <mikeob9@gmail.com>
2018-01-03 13:53:10 -0500

    Update for 201712 cruise data.

--- Data and imports/import_receiver_events.R
+++ Data and imports/import_receiver_events.R
@@ -4,28 +4,34 @@ rec_deployments <- readxl::read_excel(
 
 library(dplyr)
 rec_deployments <- rec_deployments %>%
-  filter(`Cruise ID` %in% c(201611, 201703),
+  filter(`Cruise ID` %in% c(201611, 201703, 201708),
          !is.na(`Dep VR2AR`)) %>%
   mutate(`Dep VR2AR` = paste('VR2AR', `Dep VR2AR`, sep = '-'),
          month = lubridate::month(Date)) %>%
   select(`Cruise ID`, Date, `Site ID`, `Dep VR2AR`, `Dep Lat_DD`, `Dep Long_DD`)
 
 # Load receiver events, merge with location information
-rec_events_2016 <- read.csv(
+rec_events_201703 <- read.csv(
   'p:/obrien/biotelemetry/md wea habitat/data/wea_recevents_201611_201703.csv',
   stringsAsFactors = F)
-rec_events_2017 <- read.csv(
+rec_events_201708 <- read.csv(
   'p:/obrien/biotelemetry/md wea habitat/data/wea_recevents_201703_201708.csv',
   stringsAsFactors = F)
+rec_events_201712 <- read.csv(
+  'p:/obrien/biotelemetry/md wea habitat/data/wea_recevents_201708_201712.csv',
+  stringsAsFactors = F)
 
-rec_events <- rbind(rec_events_2016, rec_events_2017)
+rec_events <- rbind(rec_events_201703, rec_events_201708, rec_events_201712)
 
 rec_events <- rec_events %>%
   distinct(Date.Time, Receiver, Description, .keep_all = T) %>%
   mutate(Date.Time = lubridate::ymd_hms(Date.Time),
          Cruise = as.numeric(ifelse(Date.Time <= '2017-03-30', 201611,
                        ifelse(Date.Time > '2017-03-30' &
-                                Date.Time <= '2017-08-27', 201703, '!!!'))))
+                                Date.Time <= '2017-08-27', 201703,
+                       ifelse(Date.Time > '2017-08-27' &
+                                Date.Time <= '2017-12-21', 201708,
+                              '!!!')))))
 
 rec_events <- left_join(rec_events, rec_deployments,
                         by = c('Receiver' = 'Dep VR2AR',
