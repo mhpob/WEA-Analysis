df8c0656eb914237f540d3228eb6aa0ef655ae4f
mhpob <mikeob9@gmail.com>
2018-01-05 11:40:15 -0500

    Adjust site data input, allow to choose between radian and degree output.

--- site-to-site bearings.R
+++ site-to-site bearings.R
@@ -1,18 +1,7 @@
-library(readxl)
-sites <- read_excel('p:/obrien/biotelemetry/md wea habitat/data/vr2ar deployment_recovery log.xlsx')
-sites <- sites[grepl('2017', sites$Date) & !is.na(sites$`Dep Long_DD`),]
+sites <- readxl::read_excel('p:/obrien/biotelemetry/md wea habitat/data/vr2ar deployment_recovery log.xlsx')
+sites <- sites[sites$`Cruise ID` == '201708' & !is.na(sites$`Dep Long_DD`),]
 
-j <- expand.grid(1:nrow(sites), 1:nrow(sites))
-
-s.combn <- data.frame('Site' = t(combn(sites$'Site ID', 2)),
-                      'Long' = t(combn(sites$'Dep Long_DD', 2)),
-                      'Lat' = t(combn(sites$'Dep Lat_DD', 2)),
-                      bearing = earth.bear(s.combn$Long.1, s.combn$Lat.1,
-                                           s.combn$Long.2, s.combn$Lat.2))
-
-
-
-earth.bear <- function (long1, lat1, long2, lat2){
+earth.bear <- function (long1, lat1, long2, lat2, radians = F){
   rad <- pi/180
   a1 <- lat1 * rad
   a2 <- long1 * rad
@@ -21,8 +10,26 @@ earth.bear <- function (long1, lat1, long2, lat2){
   dlon <- b2 - a2
   bear <- atan2(sin(dlon) * cos(b1), cos(a1) * sin(b1) - sin(a1) *
                   cos(b1) * cos(dlon))
-  deg <- (bear%%(2 * pi)) * (180/pi)
-  return(deg)
+  output <- (bear%%(2 * pi))
+  if(radians == F){
+    output <- output * (180/pi)
+  }
+  return(output)
 }
 
 
+# s.combn <- data.frame('Site' = t(combn(sites$'Site ID', 2)),
+#                       'Long' = t(combn(sites$'Dep Long_DD', 2)),
+#                       'Lat' = t(combn(sites$'Dep Lat_DD', 2)))
+# s.combn$bearing  <-  earth.bear(s.combn$Long.1, s.combn$Lat.1,
+#                                 s.combn$Long.2, s.combn$Lat.2)
+
+
+combos <- data.frame('Site' = expand.grid(sites$'Site ID', sites$'Site ID',
+                                          stringsAsFactors = F),
+                      'Long' = expand.grid(sites$'Dep Long_DD', sites$'Dep Long_DD'),
+                      'Lat' = expand.grid(sites$'Dep Lat_DD', sites$'Dep Lat_DD'))
+combos$bearing  <-  earth.bear(combos$Long.Var1, combos$Lat.Var1,
+                               combos$Long.Var2, combos$Lat.Var2, radians = T)
+names(combos) <- c('from', 'to', 'long.from', 'long.to', 'lat.from', 'lat.to',
+                   'bearing')
