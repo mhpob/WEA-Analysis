251a3cc6e446e0054b232e7fb8258a7a33cc3eac
mhpob <mikeob9@gmail.com>
2018-01-11 16:42:34 -0500

    Adjust spring to begin Mar 15.

--- temp v ecdf.R
+++ temp v ecdf.R
@@ -28,10 +28,12 @@ rec.data <- rec.data %>%
                         ifelse(grepl('O', Site), 'Outer',
                                'MD WEA')),
          Data = as.numeric(Data),
-         year = lubridate::year(Date.Time) - 2000,
-         year = ifelse(month(Date.Time) %in% 10:12,
-                       year + 1, year),
-         season = ifelse(month(Date.Time) %in% 4:9,
+         year = ifelse(month(Date.Time) %in% 10:12 |
+                         (month(Date.Time) == 9 & day(Date.Time) %in% 16:30),
+                       year(Date.Time) - 1999, year(Date.Time) - 2000),
+         season = ifelse(month(Date.Time) %in% 4:8 |
+                           (month(Date.Time) == 3 & day(Date.Time) %in% 16:31) |
+                           (month(Date.Time) == 9 & day(Date.Time) %in% 1:15),
                          'SprSum', 'AutWin'),
          season = paste0(season, year)) %>%
   group_by(Date.Time, season, array) %>%
@@ -106,105 +108,107 @@ temp_ecdfplot(array = 'MD WEA', spec.data = 'sturg')
 ## Same thing, but for season/array combinations ----
 # Edit detection data
 sp_season <- species %>%
-  mutate(year = lubridate::year(date.local) - 2000,
-         year = ifelse(lubridate::month(date.local) %in% 10:12,
-                       year + 1, year),
-         season = ifelse(lubridate::month(date.local) %in% 4:9,
+  mutate(year = ifelse(month(date.local) %in% 10:12 |
+                         (month(date.local) == 9 & day(date.local) %in% 16:30),
+                       year(date.local) - 1999, year(date.local) - 2000),
+         season = ifelse(month(date.local) %in% 4:8 |
+                           (month(date.local) == 3 & day(date.local) %in% 16:31) |
+                           (month(date.local) == 9 & day(date.local) %in% 1:15),
                          'SprSum', 'AutWin'),
          season = paste0(season, year)) %>%
   group_by(array, Common.Name, transmitter, season) %>%
   distinct(array, transmitter, .keep_all = T) %>%
   summarize(min = min(date.local))
 
-det_ecdfplot <- function(data, spec.plot, array,
-                         season.plot = NULL, ylab = NULL, ...){
-
-  data <- filter(data,
-                 grepl(spec.plot, Common.Name, ignore.case = T),
-                 season == season.plot)
-  if(is.null(season.plot)){
-    data <- split(data, data$array)
-  } else{
-    data <- split(data, list(data$array, data$season))
-  }
-  data <- lapply(data, function(x){ecdf(x$min)})
-
-  date_lims <- if(grepl('Aut', season.plot)){
-    ymd_hms(c(paste0('20', as.numeric(substr(season.plot, 7, 8)) - 1,
-                     '1101 00:00:00'),
-              paste0('20', substr(season.plot, 7, 8), '0331 23:59:59')),
-            tz = 'America/New_York')
-  } else {
-    ymd_hms(c(paste0('20', substr(season.plot, 7, 8), '0401 00:00:00'),
-              paste0('20', substr(season.plot, 7, 8), '0930 23:59:59')),
-            tz = 'America/New_York')
-  }
-
-  subset <- grep(array, names(data))
-
-  plot(x = as_datetime(knots(data[[subset]])),
-       y = data[[subset]](knots(data[[subset]])),
-       ylim = c(0, 1),
-       xlim = date_lims,
-       xlab = 'Date',
-       ylab = ifelse(is.null(ylab),
-                     eval(expression(paste(
-                       'Fraction detected in', array, 'array,', season.plot))),
-                     ylab),
-       pch = 16, ...)
-
-  # This is just to stop lines() from throwing warnings when we specify
-  # graphical parameters it doesn't like.
-  LLines <- function(..., log, axes, frame.plot, panel.first, panel.last) {
-    lines(...)
-  }
-  LLines(x = as_datetime(knots(data[[subset]])),
-         y = data[[subset]](knots(data[[subset]])),
-         type = 's', ...)
-}
-det_ecdfplot(data = sp_season, 'str', 'MD WEA', season.plot = 'SprSum17')
-
-temp_ecdfplot <- function(spec.data, array, spec.plot, season.plot){
-  temp.data <- rec.data[rec.data$array == array &
-                          rec.data$season == season.plot,]
-
-  date_lims <- if(grepl('Aut', season.plot)){
-    ymd_hms(c(paste0('20', as.numeric(substr(season.plot, 7, 8)) - 1,
-                     '1001 00:00:00'),
-              paste0('20', substr(season.plot, 7, 8), '0331 23:59:59')),
-            tz = 'America/New_York')
-  } else {
-    ymd_hms(c(paste0('20', substr(season.plot, 7, 8), '0401 00:00:00'),
-              paste0('20', substr(season.plot, 7, 8), '0930 23:59:59')),
-            tz = 'America/New_York')
-  }
-
-  par(mar = c(4, 4, 1, 4) + 0.1)
-  plot(x = temp.data$Date.Time,
-       y = temp.data$avg.temp,
-       xlim = date_lims,
-       # ylim = c(4.5, 16.5),
-       xaxt = 'n',
-       xlab = 'Date',
-       ylab = 'Temperature (C)',
-       type = 'l',
-       lwd = 2)
-  axis.POSIXct(1, at = seq(date_lims[1], date_lims[2], by = 'month'),
-               format = '%m-%Y')
-
-  par(new = T)
-  det_ecdfplot(data = spec.data, spec.plot = spec.plot, array = array,
-               season.plot = season.plot, ylab = '', axes = F, col = 'blue')
-  axis(4, las = 0.5, col.axis = 'blue')
-  mtext(side = 4, line = 2, text = eval(expression(paste(
-    'Fraction detected in', array, 'array,', season.plot))), col = 'blue')
-}
-temp_ecdfplot(spec.data = sp_season, array = 'MD WEA',
-              spec.plot = 'str', season.plot = 'AutWin17')
-temp_ecdfplot(spec.data = sp_season, array = 'MD WEA',
-              spec.plot = 'str', season.plot = 'SprSum17')
-temp_ecdfplot(spec.data = sp_season, array = 'MD WEA',
-              spec.plot = 'str', season.plot = 'AutWin18')
+# det_ecdfplot <- function(data, spec.plot, array,
+#                          season.plot = NULL, ylab = NULL, ...){
+#
+#   data <- filter(data,
+#                  grepl(spec.plot, Common.Name, ignore.case = T),
+#                  season == season.plot)
+#   if(is.null(season.plot)){
+#     data <- split(data, data$array)
+#   } else{
+#     data <- split(data, list(data$array, data$season))
+#   }
+#   data <- lapply(data, function(x){ecdf(x$min)})
+#
+#   date_lims <- if(grepl('Aut', season.plot)){
+#     ymd_hms(c(paste0('20', as.numeric(substr(season.plot, 7, 8)) - 1,
+#                      '1101 00:00:00'),
+#               paste0('20', substr(season.plot, 7, 8), '0331 23:59:59')),
+#             tz = 'America/New_York')
+#   } else {
+#     ymd_hms(c(paste0('20', substr(season.plot, 7, 8), '0401 00:00:00'),
+#               paste0('20', substr(season.plot, 7, 8), '0930 23:59:59')),
+#             tz = 'America/New_York')
+#   }
+#
+#   subset <- grep(array, names(data))
+#
+#   plot(x = as_datetime(knots(data[[subset]])),
+#        y = data[[subset]](knots(data[[subset]])),
+#        ylim = c(0, 1),
+#        xlim = date_lims,
+#        xlab = 'Date',
+#        ylab = ifelse(is.null(ylab),
+#                      eval(expression(paste(
+#                        'Fraction detected in', array, 'array,', season.plot))),
+#                      ylab),
+#        pch = 16, ...)
+#
+#   # This is just to stop lines() from throwing warnings when we specify
+#   # graphical parameters it doesn't like.
+#   LLines <- function(..., log, axes, frame.plot, panel.first, panel.last) {
+#     lines(...)
+#   }
+#   LLines(x = as_datetime(knots(data[[subset]])),
+#          y = data[[subset]](knots(data[[subset]])),
+#          type = 's', ...)
+# }
+# det_ecdfplot(data = sp_season, 'str', 'MD WEA', season.plot = 'SprSum17')
+#
+# temp_ecdfplot <- function(spec.data, array, spec.plot, season.plot){
+#   temp.data <- rec.data[rec.data$array == array &
+#                           rec.data$season == season.plot,]
+#
+#   date_lims <- if(grepl('Aut', season.plot)){
+#     ymd_hms(c(paste0('20', as.numeric(substr(season.plot, 7, 8)) - 1,
+#                      '1001 00:00:00'),
+#               paste0('20', substr(season.plot, 7, 8), '0331 23:59:59')),
+#             tz = 'America/New_York')
+#   } else {
+#     ymd_hms(c(paste0('20', substr(season.plot, 7, 8), '0401 00:00:00'),
+#               paste0('20', substr(season.plot, 7, 8), '0930 23:59:59')),
+#             tz = 'America/New_York')
+#   }
+#
+#   par(mar = c(4, 4, 1, 4) + 0.1)
+#   plot(x = temp.data$Date.Time,
+#        y = temp.data$avg.temp,
+#        xlim = date_lims,
+#        # ylim = c(4.5, 16.5),
+#        xaxt = 'n',
+#        xlab = 'Date',
+#        ylab = 'Temperature (C)',
+#        type = 'l',
+#        lwd = 2)
+#   axis.POSIXct(1, at = seq(date_lims[1], date_lims[2], by = 'month'),
+#                format = '%m-%Y')
+#
+#   par(new = T)
+#   det_ecdfplot(data = spec.data, spec.plot = spec.plot, array = array,
+#                season.plot = season.plot, ylab = '', axes = F, col = 'blue')
+#   axis(4, las = 0.5, col.axis = 'blue')
+#   mtext(side = 4, line = 2, text = eval(expression(paste(
+#     'Fraction detected in', array, 'array,', season.plot))), col = 'blue')
+# }
+# temp_ecdfplot(spec.data = sp_season, array = 'MD WEA',
+#               spec.plot = 'str', season.plot = 'AutWin17')
+# temp_ecdfplot(spec.data = sp_season, array = 'MD WEA',
+#               spec.plot = 'str', season.plot = 'SprSum17')
+# temp_ecdfplot(spec.data = sp_season, array = 'MD WEA',
+#               spec.plot = 'str', season.plot = 'AutWin18')
 
 
 d_ecdfplot <- function(data, spec.plot, array,
@@ -277,5 +281,7 @@ t_ecdfplot <- function(spec.data, array, spec.plot){
 }
 
 t_ecdfplot(spec.data = sp_season, array = 'Inner', spec.plot = 'c stur')
+
+
+
 t_ecdfplot(spec.data = sp_season, array = 'MD WEA', spec.plot = 'str')
-t_ecdfplot(spec.data = sp_season, array = 'Outer', spec.plot = 'White')
