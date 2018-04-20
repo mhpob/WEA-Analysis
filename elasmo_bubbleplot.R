2aff1dfbc9fc996904fe1caae3e0698274b6ccf8
mhpob <mikeob9@gmail.com>
2018-04-20 16:07:15 -0400

    Add dusky sharks, switch to sf from raster.

--- elasmo_bubbleplot.R
+++ elasmo_bubbleplot.R
@@ -1,4 +1,4 @@
-library(TelemetryR); library(lubridate);library(dplyr)
+library(sf); library(ggplot2); library(TelemetryR); library(lubridate);library(dplyr)
 dets <- vemsort('p:/obrien/biotelemetry/detections/offshore md/fish migration')
 load('p:/obrien/randomr/ACTactive.rda')
 
@@ -8,6 +8,8 @@ elasmo <- left_join(data.frame(dets), ACTactive,
                Common.Name, ignore.case = T)) %>%
   mutate(Common.Name = tolower(Common.Name))
 
+
+# Great White ----
 plot_data <- elasmo %>%
   filter(Common.Name == 'white shark') %>%
   mutate(mnth = month(date.local, label = T, abbr = T),
@@ -22,15 +24,9 @@ plot_data <- rbind(data.frame(plot_data), data.frame(mnth = 'Jan', station = NA,
                                                      lat = NA, long = NA, n =NA))
 
 
-library(raster)
-midstates <- shapefile('p:/obrien/midatlantic/matl_states_land.shp')
-wea <- shapefile('c:/users/secor/desktop/gis products/md mammals/wind_planning_areas/Wind_Planning_Areas_06_20_2014.shp')
+midstates <- st_read('p:/obrien/midatlantic/matl_states_land.shp')
+wea <- st_read('c:/users/secor/desktop/gis products/md mammals/wind_planning_areas/Wind_Planning_Areas_06_20_2014.shp')
 
-
-
-library(ggplot2)
-midstates <- fortify(midstates)
-wea <- fortify(wea)
 base_map <- ggplot() +
   geom_polygon(data = midstates, aes(x = long, y = lat, group = group),
                fill  = 'lightgrey', color = 'black') +
@@ -46,4 +42,33 @@ base_map +
   labs(title = 'Unique Great White Sharks', size = 'Number') +
   facet_wrap(~ mnth, ncol = 4) +
   theme_bw() +
+  theme(legend.position = c(0.85, 0.15))
+
+
+# Dusky sharks
+plot_data <- elasmo %>%
+  filter(Common.Name == 'dusky shark') %>%
+  mutate(mnth = month(date.local, label = T, abbr = T),
+         mnth = factor(mnth, levels = c('Nov', 'Dec', 'Jan', 'Feb', 'Mar',
+                                        'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep','Oct')))%>%
+  distinct(mnth, transmitter, station, lat, long) %>%
+  group_by(mnth, station, lat, long) %>%
+  summarize(n = n())
+
+base_map <- ggplot() +
+  geom_sf(data = midstates,
+               fill  = 'lightgrey', color = 'black') +
+  geom_sf(data = wea,
+               fill = NA, color = 'black') +
+  coord_sf(xlim = c(-75.2, -74.45), ylim = c(38.2, 38.5), datum = NA) +
+  labs(x = NULL, y = NULL)
+
+base_map +
+  geom_point(data = plot_data,
+             aes(x = long, y = lat, size = n),
+             color = 'red') +
+  labs(title = 'Unique Dusky Sharks', size = 'Number') +
+  facet_wrap(~ mnth, ncol = 3) +
+  theme_bw()
++
   theme(legend.position = c(0.85, 0.15))
