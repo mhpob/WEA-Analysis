024c5108d7011fee57c8dd5b3ca19ba11d3259c2
mhpob <mikeob9@gmail.com>
2020-07-02 11:28:19 -0400

    Plotting changes.

--- temperature comparison.R
+++ temperature comparison.R
@@ -4,11 +4,16 @@ library(dplyr)
 
 
 data <- readRDS('data and imports/rangetest_logit_binary.RDS') %>%
-  rename(bwt = `Average temperature`) %>%
+  rename(bwt = `Average temperature`,
+         noise = `Average noise`) %>%
   distinct(date, array, .keep_all = T) %>%
-  pivot_longer(cols = c(dt, sst, bwt), names_to = 'type') %>%
-  mutate(group = ifelse(type == 'dt', "Delta*T", 'Observed'),
-         group = factor(group, levels = c('Observed', "Delta*T"), ordered = T),
+  pivot_longer(cols = c(dt, sst, bwt, noise), names_to = 'type') %>%
+  mutate(group = case_when(type == 'dt' ~ "Delta*T~(degree*C)",
+                           type == 'noise' ~ 'Average~noise~(mV)',
+                           T ~ 'Observed~temperature~(degree*C)'),
+         group = factor(group, levels = c('Observed~temperature~(degree*C)',
+                                          "Delta*T~(degree*C)",
+                                          'Average~noise~(mV)'), ordered = T),
          type = toupper(type),
          site = ifelse(array == 'Inner', 'Nearshore', 'Offshore'))
 
@@ -19,10 +24,11 @@ ggplot() +
   geom_line(data = data,
             aes(x = date, y = value, lty = site, color = type)) +
   scale_color_manual(breaks = c('SST', 'BWT'),
-                     values = c('blue', 'black', 'red')) +
+                     values = c('blue', 'black', 'black', 'red')) +
   facet_wrap(~ group, ncol = 1,
-             labeller = label_parsed) +
-  labs(x = NULL, y = 'Temperature (C)') +
+             labeller = label_parsed,
+             strip.position = 'right', scales = 'free_y') +
+  labs(x = NULL, y = NULL) +
   theme_bw() +
   theme(strip.text = element_text(size = 12),
         axis.text = element_text(size = 11),
@@ -32,3 +38,7 @@ ggplot() +
         legend.box = 'horizontal',
         legend.margin = margin(0, 2, 2,2),
         legend.text = element_text(size = 12))
+
+
+names(data)
+ggplot() +geom_line(data=data, aes(x = date, y = `Average noise`, lty = site))
